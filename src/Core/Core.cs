34b98877b70ba6c08522c61ad9da784b8cf4f3c2
Stridemann <stridemann@gmail.com>
2018-02-27 05:22:42 +0200

    Visual rework, bug fixes, improvements

--- src/Core/Core.cs
+++ src/Core/Core.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Windows.Forms;
+using Developer_Tool;
 using ImGuiNET;
 using PoeHUD.Controllers;
 using PoeHUD.DebugPlug;
@@ -17,40 +18,36 @@ using SharpDX;
 using ImGuiVector2 = System.Numerics.Vector2;
 using ImGuiVector4 = System.Numerics.Vector4;
 
-namespace DeveloperTool.Core
+namespace SithImGuiDev.Core
 {
-    public class DeveloperTool : BaseSettingsPlugin<Settings>
+    public class Core : BaseSettingsPlugin<DevSettings>
     {
-        private static ImGuiVector2 _renderDebugwindowSize = new ImGuiVector2(784, API.GameController.Window.GetWindowRectangle().Height - 129);
+        private static ImGuiVector2 _renderDebugwindowSize = new ImGuiVector2(784, API.GameController.Window.GetWindowRectangle().Height-129);
         private static ImGuiVector2 _renderDebugwindowPos = new ImGuiVector2(593, 0);
         private static readonly ImGuiVector2 RenderDebugnextWindowPos = new ImGuiVector2(_renderDebugwindowSize.X + _renderDebugwindowPos.X, _renderDebugwindowSize.Y + _renderDebugwindowPos.Y);
-        public static DeveloperTool Instance;
-        private readonly List<(string name, object obj)> _objectForDebug = new List<(string name, object obj)>();
-        private readonly List<RectangleF> _rectForDebug = new List<RectangleF>();
+        public static Core Instance;
         private Color _clr = Color.Pink;
         private Coroutine _coroutineRndColor;
         private bool _enableDebugHover;
         private GameController _gameController;
+        private readonly List<(string name, object obj)> _objectForDebug = new List<(string name, object obj)>();
+        private readonly List<RectangleF> _rectForDebug = new List<RectangleF>();
         private Random _rnd;
-        private Settings _settings;
+        private DevSettings _settings;
         private long _uniqueIndex;
 
-        public DeveloperTool() => PluginName = "Qvin Debug Tree";
+        // Examples apps
+
+        public Core() => PluginName = "Qvin Debug Tree";
 
         public override void Initialise()
         {
             base.Initialise();
             Instance = this;
             _gameController = GameController;
-            GameController.Area.OnAreaChange += area => AreaChange();
             _settings = Settings;
             _rnd = new Random((int) _gameController.MainTimer.ElapsedTicks);
-            _coroutineRndColor = new Coroutine(() => { _clr = new Color(_rnd.Next(255), _rnd.Next(255), _rnd.Next(255), 255); }, new WaitTime(200), nameof(DeveloperTool), "Random Color").Run();
-            DebugObjects();
-        }
-
-        private void DebugObjects()
-        {
+            _coroutineRndColor = new Coroutine(() => { _clr = new Color(_rnd.Next(255), _rnd.Next(255), _rnd.Next(255), 255); }, new WaitTime(200), nameof(Core), "Random Color").Run();
             _objectForDebug.Add(("LocalPlayer", GameController.Game.IngameState.Data.LocalPlayer));
             _objectForDebug.Add(("GameController", GameController));
             _objectForDebug.Add(("GameController.Game", GameController.Game));
@@ -62,12 +59,6 @@ namespace DeveloperTool.Core
         {
             RenderDebugInformation();
             //ImGuiNative.igShowDemoWindow(ref _tempBool);
-        }
-
-        private void AreaChange()
-        {
-            _objectForDebug.Clear();
-            DebugObjects();
         }
 
         private void RenderDebugInformation()
@@ -82,9 +73,7 @@ namespace DeveloperTool.Core
                 foreach (var rectangleF in _rectForDebug) Graphics.DrawFrame(rectangleF, 2, _clr);
                 ImGui.SetNextWindowPos(RenderDebugnextWindowPos, Condition.Appearing, new ImGuiVector2(1, 1));
                 ImGui.SetNextWindowSize(_renderDebugwindowSize, Condition.Appearing);
-                var isOpen = Settings.ShowWindow.Value;
-                ImGui.BeginWindow("DebugTree", ref isOpen, WindowFlags.Default);
-                Settings.ShowWindow.Value = isOpen;
+                ImGui.BeginWindow("DebugTree");
                 if (ImGui.Button("Clear##base")) _rectForDebug.Clear();
                 ImGui.SameLine();
                 ImGui.Checkbox("F1 for debug hover", ref _enableDebugHover);
@@ -130,8 +119,8 @@ namespace DeveloperTool.Core
                     }
                     else
                     {
-                        ro = (Entity) obj;
-                        comp = ((Entity) obj).GetComponents();
+                        ro = (Entity)obj;
+                        comp = ((Entity)obj).GetComponents();
                     }
 
                     if (ImGui.TreeNode($"Components {comp.Count} ##{ro.GetHashCode()}"))
@@ -177,17 +166,44 @@ namespace DeveloperTool.Core
                 {
                     ImGui.SameLine();
                     _uniqueIndex++;
-                    if (ImGui.Button($"Draw this##{_uniqueIndex}")) _rectForDebug.Add(el1.GetClientRect());
+                    if (ImGui.Button($"Draw this##{_uniqueIndex}"))
+                    {
+                        _rectForDebug.Add(el1.GetClientRect());
+                    }
+
                     ImGui.SameLine();
                     _uniqueIndex++;
                     if (ImGui.Button($"Clear##from draw this{_uniqueIndex}")) _rectForDebug.Clear();
                 }
 
                 var oProp = obj.GetType().GetProperties(flags).Where(x => x.GetIndexParameters().Length == 0);
-                //DebugImGuiFields(obj);
-                oProp = oProp.OrderBy(x => x.Name).ToList();
+                var ordered1 = oProp.OrderBy(x => x.PropertyType.GetInterfaces().Contains(typeof(IEnumerable)));//We want to show arrays and lists last
+
+                oProp = ordered1.ThenBy(x => x.Name).ToList();
                 foreach (var propertyInfo in oProp)
-                    if (propertyInfo.GetValue(obj, null).GetType().IsPrimitive || propertyInfo.GetValue(obj, null) is decimal || propertyInfo.GetValue(obj, null) is string || propertyInfo.GetValue(obj, null) is TimeSpan || propertyInfo.GetValue(obj, null) is Enum)
+                {
+
+                    if (propertyInfo.ReflectedType.IsSubclassOf(typeof(RemoteMemoryObject)))//We don't need to see this shit
+                    {
+                        if (propertyInfo.Name == "M" ||
+                            propertyInfo.Name == "Game" ||
+                            propertyInfo.Name == "Offsets")
+                            continue;
+                    }
+
+                    if (propertyInfo.ReflectedType.IsSubclassOf(typeof(Component)))//...and this one too
+                    {
+                        if (propertyInfo.Name == "Owner")
+                            continue;
+                    }
+
+                    if (
+                        //propertyInfo.GetValue(obj, null).GetType().IsPrimitive  //Wanna get null or what?
+                        propertyInfo.PropertyType.IsPrimitive
+                     || propertyInfo.GetValue(obj, null) is decimal
+                     || propertyInfo.GetValue(obj, null) is string
+                     || propertyInfo.GetValue(obj, null) is TimeSpan
+                     || propertyInfo.GetValue(obj, null) is Enum)
                     {
                         ImGui.Text($"{propertyInfo.Name}: ");
                         ImGui.SameLine();
@@ -195,17 +211,17 @@ namespace DeveloperTool.Core
                         if (propertyInfo.Name.Contains("Address"))
                             o = Convert.ToInt64(o).ToString("X");
                         ImGui.Text($"{o}", new ImGuiVector4(1, 0.647f, 0, 1));
-                        if (!propertyInfo.Name.Contains("Address")) continue;
+                        //if (!propertyInfo.Name.Contains("Address")) continue; //We want to copy any thing we need
                         ImGui.SameLine();
                         if (ImGui.SmallButton($"Copy##{o}")) ImGuiNative.igSetClipboardText(o.ToString());
                     }
                     else
                     {
-                        var label = propertyInfo.ToString();
+                        var label = propertyInfo.Name;
                         var o = propertyInfo.GetValue(obj, null);
                         if (o == null)
                         {
-                            ImGui.Text("Null");
+                            ImGui.Text(label + ": Null");
                             continue;
                         }
 
@@ -215,7 +231,8 @@ namespace DeveloperTool.Core
                             if (ImGui.TreeNode(label))
                             {
                                 _uniqueIndex++;
-                                if (ImGui.Button($"Debug this##{_uniqueIndex}"))
+                                ImGui.SameLine();
+                                if (ImGui.SmallButton($"Debug this##{_uniqueIndex}"))
                                 {
                                     var formattable = $"{label}->{o}";
                                     if (_objectForDebug.Any(x => x.name.Contains(formattable)))
@@ -232,53 +249,65 @@ namespace DeveloperTool.Core
                             }
 
                         if (!propertyInfo.PropertyType.GetInterfaces().Contains(typeof(IEnumerable))) continue;
-                        ImGui.Text($"{propertyInfo.GetValue(obj, null)}", new ImGuiVector4(0.486f, 0.988f, 0, 1));
-                        var i = 0;
-                        var enumerable = (IEnumerable) o;
-                        var items = enumerable as IList<object> ?? enumerable.Cast<object>().ToList();
-                        _uniqueIndex++;
-                        if (ImGui.Button($"Draw Childs##{_uniqueIndex}"))
+                        if (ImGui.TreeNode($"{propertyInfo.Name}:"))    //Hide arrays to tree node
                         {
-                            var tempi = 0;
+                            var enumerable = (IEnumerable)o;
+                            var items = enumerable as IList<object> ?? enumerable.Cast<object>().ToList();
+
+                            var gArgs = o.GetType().GenericTypeArguments.ToList();
+                            if (gArgs.Any(x => x == typeof(Element) || x.IsSubclassOf(typeof(Element))))    //We need to draw it ONLY for UI Elements
+                            {
+                                _uniqueIndex++;
+                                if (ImGui.Button($"Draw Childs##{_uniqueIndex}"))
+                                {
+                                    var tempi = 0;
+                                    foreach (var item in items)
+                                    {
+                                        var el = (Element)item;
+                                        _rectForDebug.Add(el.GetClientRect());
+                                        tempi++;
+                                        if (tempi > 1000) break;
+                                    }
+                                }
+
+                                ImGui.SameLine();
+                                _uniqueIndex++;
+                                if (ImGui.Button($"Draw Childs for Childs##{_uniqueIndex}")) DrawChilds(items);
+                                ImGui.SameLine();
+                                _uniqueIndex++;
+                                if (ImGui.Button($"Draw Childs for Childs Only Visible##{_uniqueIndex}")) DrawChilds(items, true);
+                                ImGui.SameLine();
+                                _uniqueIndex++;
+                                if (ImGui.Button($"Clear##from draw childs##{_uniqueIndex}")) _rectForDebug.Clear();
+
+                            }
+
+                            var i = 0;
                             foreach (var item in items)
                             {
-                                var el = (Element) item;
-                                _rectForDebug.Add(el.GetClientRect());
-                                tempi++;
-                                if (tempi > 1000) break;
+                                if (item == null)
+                                {
+                                    ImGui.Text($"Null", new ImGuiVector4(0.486f, 0.988f, 0, 1));
+                                    continue;
+                                }
+                                if (i > 500) break;
+                              
+                               
+                                if (ImGui.TreeNode($"Index: {i}"))//Draw only index
+                                {
+                                    DebugForImgui(item);
+                                    ImGui.TreePop();
+                                }
+                                ImGui.SameLine();
+                                ImGui.Text($"{item}", new ImGuiVector4(0.486f, 0.988f, 0, 1));
+                                i++;
                             }
-                        }
-
-                        ImGui.SameLine();
-                        _uniqueIndex++;
-                        if (ImGui.Button($"Draw Childs for Childs##{_uniqueIndex}")) DrawChilds(items);
-                        ImGui.SameLine();
-                        _uniqueIndex++;
-                        if (ImGui.Button($"Draw Childs for Childs Only Visible##{_uniqueIndex}")) DrawChilds(items, true);
-                        ImGui.SameLine();
-                        _uniqueIndex++;
-                        if (ImGui.Button($"Clear##from draw childs##{_uniqueIndex}")) _rectForDebug.Clear();
-                        foreach (var item in items)
-                        {
-                            if (item == null)
-                                continue;
-                            if (i > 500) break;
-                            ImGui.Text($"{item}", new ImGuiVector4(0.486f, 0.988f, 0, 1));
-                            ImGui.SameLine();
-                            if (ImGui.TreeNode($" #{i} ##{item}  {item.GetHashCode()}"))
-                            {
-                                DebugForImgui(item);
-                                ImGui.TreePop();
-                            }
-
-                            i++;
+                            ImGuiNative.igUnindent();
                         }
                     }
+                }
             }
-            catch (Exception e)
-            {
-                DebugPlugin.LogMsg($"Debug Tree: {e.Message}", 1);
-            }
+            catch (Exception e) { DebugPlugin.LogMsg($"Debug Tree: {e.ToString()}", 1); }
         }
 
         private void DrawChilds(object obj, bool onlyVisible = false)
@@ -302,7 +331,10 @@ namespace DeveloperTool.Core
             }
             else
             {
-                if (obj is Element el) _rectForDebug.Add(el.GetClientRect());
+                if (obj is Element el)
+                {
+                    _rectForDebug.Add(el.GetClientRect());
+                }
             }
         }
 
